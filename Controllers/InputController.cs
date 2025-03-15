92eab8551f5754817688b17ee7edcbb3f06220f9
LeTran <lethanhbaotran.letran@gmail.com>
2025-03-15 10:02:48 +0700

    controllercontroller

--- Controllers/InputController.cs
+++ Controllers/InputController.cs
@@ -1,32 +1,42 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using DADN.Models;
+using System.Text.Json;
 
-namespace DADN.Controllers;
-
-public class InputController : Controller
+namespace DADN.Controllers
 {
-    private readonly ILogger<InputController> _logger;
-
-    public InputController(ILogger<InputController> logger)
+    public class InputController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<InputController> _logger;
 
-    public IActionResult Index()
-    {
-        // _logger.LogInformation("Home page visited");
-        return View();
-    }
+        public InputController(ILogger<InputController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Test()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpPost]
+        public IActionResult Submit([FromBody] InputModel input)
+        {
+            if (input == null)
+            {
+                return BadRequest(new { message = "Dữ liệu không hợp lệ!" });
+            }
+
+            _logger.LogInformation($"Dữ liệu nhận: {JsonSerializer.Serialize(input)}");
+
+            // Trả về dữ liệu để frontend lưu vào Local Storage
+            return Ok(input);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
