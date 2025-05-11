6b79bf7e7b48076676694aa3bcaed709ccef62f9
vck2004 <52705258+vck2004@users.noreply.github.com>
2025-05-12 00:07:44 +0700

    dashboard complete

--- wwwroot/assets/static/js/pages/dashboard.js
+++ wwwroot/assets/static/js/pages/dashboard.js
@@ -1,158 +1,207 @@
-var optionsProfileVisit = {
-  annotations: {
-    position: "back",
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  chart: {
-    type: "bar",
-    height: 300,
-  },
-  fill: {
-    opacity: 1,
-  },
-  plotOptions: {},
-  series: [
-    {
-      name: "sales",
-      data: [9, 20, 30, 20, 10, 20, 30, 20, 10, 20, 30, 20],
-    },
-  ],
-  colors: "#435ebe",
-  xaxis: {
-    categories: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ],
-  },
+// Fetch motor counts by type
+async function fetchMotorCounts() {
+    try {
+        const response = await fetch('/api/motors/count-by-type');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const types = Object.keys(data);
+        const counts = Object.values(data);
+        const totalCount = counts.reduce((total, count) => total + count, 0);
+
+        // Calculate percentages for each motor type
+        const percentages = counts.map(count => ((count / totalCount) * 100));
+        
+        renderMotorTypeChart(types, percentages);
+        
+        return data;
+    } catch (error) {
+        console.error("Error fetching motor counts:", error);
+        return {};
+    }
 }
-let optionsVisitorsProfile = {
-  series: [70, 30],
-  labels: ["Male", "Female"],
-  colors: ["#435ebe", "#55c6e8"],
-  chart: {
-    type: "donut",
-    width: "100%",
-    height: "350px",
-  },
-  legend: {
-    position: "bottom",
-  },
-  plotOptions: {
-    pie: {
-      donut: {
-        size: "30%",
+function renderMotorTypeChart(types, counts) {
+    let optionsVisitorsProfile = {
+      series: counts,
+      labels: types,
+      colors: ["#435ebe", "#55c6e8"],
+      chart: {
+        type: "donut",
+        width: "100%",
+        height: "350px",
       },
-    },
-  },
+      legend: {
+        position: "bottom",
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            size: "30%",
+          },
+        },
+      },
+    }
+  let chartVisitorsProfile = new ApexCharts(
+    document.getElementById("chart-visitors-profile"),
+    optionsVisitorsProfile
+  )
+  chartVisitorsProfile.render()
 }
+document.addEventListener('DOMContentLoaded', fetchMotorCounts);
 
-// var optionsEurope = {
-//   series: [
-//     {
-//       name: "series1",
-//       data: [310, 800, 600, 430, 540, 340, 605, 805, 430, 540, 340, 605],
-//     },
-//   ],
-//   chart: {
-//     height: 80,
-//     type: "area",
-//     toolbar: {
-//       show: false,
-//     },
-//   },
-//   colors: ["#5350e9"],
-//   stroke: {
-//     width: 2,
-//   },
-//   grid: {
-//     show: false,
-//   },
-//   dataLabels: {
-//     enabled: false,
-//   },
-//   xaxis: {
-//     type: "datetime",
-//     categories: [
-//       "2018-09-19T00:00:00.000Z",
-//       "2018-09-19T01:30:00.000Z",
-//       "2018-09-19T02:30:00.000Z",
-//       "2018-09-19T03:30:00.000Z",
-//       "2018-09-19T04:30:00.000Z",
-//       "2018-09-19T05:30:00.000Z",
-//       "2018-09-19T06:30:00.000Z",
-//       "2018-09-19T07:30:00.000Z",
-//       "2018-09-19T08:30:00.000Z",
-//       "2018-09-19T09:30:00.000Z",
-//       "2018-09-19T10:30:00.000Z",
-//       "2018-09-19T11:30:00.000Z",
-//     ],
-//     axisBorder: {
-//       show: false,
-//     },
-//     axisTicks: {
-//       show: false,
-//     },
-//     labels: {
-//       show: false,
-//     },
-//   },
-//   show: false,
-//   yaxis: {
-//     labels: {
-//       show: false,
-//     },
-//   },
-//   tooltip: {
-//     x: {
-//       format: "dd/MM/yy HH:mm",
-//     },
-//   },
-// }
+async function fetchMotorEffi() {
+    try {
+        const response = await fetch('/api/motors/filter-catalogs-by-efficiency');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const names = data.map(ele => ele.range);
+        const counts = data.map(ele => ele.count);
+        renderMotorEffiChart(names, counts);
+        
+        return data;
+    } catch (error) {
+        console.error("Error fetching motor counts:", error);
+        return {};
+    }
+}
+function renderMotorEffiChart(types, counts) {
+    let optionsProfileVisit = {
+      annotations: {
+        position: "back",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      chart: {
+        type: "bar",
+        height: 300,
+      },
+      fill: {
+        opacity: 1,
+      },
+      plotOptions: {},
+      series: [
+        {
+          name: "amount",
+          data: counts,
+        },
+      ],
+      colors: "#435ebe",
+      xaxis: {
+        categories: types,
+      },
+    }
+  let chartProfileVisit = new ApexCharts(
+    document.querySelector("#chart-profile-visit"),
+    optionsProfileVisit
+  )
+  chartProfileVisit.render()
+}
+document.addEventListener('DOMContentLoaded', fetchMotorEffi);
 
-// let optionsAmerica = {
-//   ...optionsEurope,
-//   colors: ["#008b75"],
-// }
-// let optionsIndonesia = {
-//   ...optionsEurope,
-//   colors: ["#dc3545"],
-// }
+async function fetchMotorHP() {
+    try {
+        const response = await fetch('/api/motors/filter-catalogs-by-output-hp');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const names = data.map(ele => ele.range);
+        const counts = data.map(ele => ele.count);
+        renderMotorHPChart(names, counts);
+        
+        return data;
+    } catch (error) {
+        console.error("Error fetching motor counts:", error);
+        return {};
+    }
+}
+function renderMotorHPChart(types, counts) {
+    let optionsProfileVisit = {
+      annotations: {
+        position: "back",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      chart: {
+        type: "bar",
+        height: 300,
+      },
+      fill: {
+        opacity: 1,
+      },
+      plotOptions: {},
+      series: [
+        {
+          name: "amount",
+          data: counts,
+        },
+      ],
+      colors: "#435ebe",
+      xaxis: {
+        categories: types,
+      },
+    }
+  let chartProfileVisit = new ApexCharts(
+    document.querySelector("#chart-profile-visit1"),
+    optionsProfileVisit
+  )
+  chartProfileVisit.render()
+}
+document.addEventListener('DOMContentLoaded', fetchMotorHP);
 
-var chartProfileVisit = new ApexCharts(
-  document.querySelector("#chart-profile-visit"),
-  optionsProfileVisit
-)
-var chartVisitorsProfile = new ApexCharts(
-  document.getElementById("chart-visitors-profile"),
-  optionsVisitorsProfile
-)
-// var chartEurope = new ApexCharts(
-//   document.querySelector("#chart-europe"),
-//   optionsEurope
-// )
-// var chartAmerica = new ApexCharts(
-//   document.querySelector("#chart-america"),
-//   optionsAmerica
-// )
-// var chartIndonesia = new ApexCharts(
-//   document.querySelector("#chart-indonesia"),
-//   optionsIndonesia
-// )
-
-// chartIndonesia.render()
-// chartAmerica.render()
-// chartEurope.render()
-chartProfileVisit.render()
-chartVisitorsProfile.render()
+async function fetchMotorRPM() {
+    try {
+        const response = await fetch('/api/motors/filter-catalogs-by-full-load-rpm');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const names = data.map(ele => ele.range);
+        const counts = data.map(ele => ele.count);
+        renderMotorRPMChart(names, counts);
+        
+        return data;
+    } catch (error) {
+        console.error("Error fetching motor counts:", error);
+        return {};
+    }
+}
+function renderMotorRPMChart(types, counts) {
+    let optionsProfileVisit = {
+      annotations: {
+        position: "back",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      chart: {
+        type: "bar",
+        height: 300,
+      },
+      fill: {
+        opacity: 1,
+      },
+      plotOptions: {},
+      series: [
+        {
+          name: "amount",
+          data: counts,
+        },
+      ],
+      colors: "#435ebe",
+      xaxis: {
+        categories: types,
+      },
+    }
+  let chartProfileVisit = new ApexCharts(
+    document.querySelector("#chart-profile-visit2"),
+    optionsProfileVisit
+  )
+  chartProfileVisit.render()
+}
+document.addEventListener('DOMContentLoaded', fetchMotorRPM);
